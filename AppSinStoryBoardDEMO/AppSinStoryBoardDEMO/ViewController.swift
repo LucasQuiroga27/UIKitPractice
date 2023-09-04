79773dd68e78615df1e3aa951115aa5e13109de3
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-09-03 21:32:55 -0500

    APPLICATION WITHOUT STORYBOARD

---
+++ AppSinStoryBoardDEMO/AppSinStoryBoardDEMO/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AppSinStoryBoardDEMO
+//
+//  Created by Lucas Quiroga on 3/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGray
+    }
+
+
+}
+
